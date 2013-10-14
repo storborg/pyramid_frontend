8d6215d8718ad0013ff1678490cf8146c0234b56
Scott Torborg <scott@cartlogic.com>
2013-10-14 14:10:30 -0700

    Add a functional test which uses request.image_url() and then fetches the specified image

--- pyramid_frontend/tests/app.py
+++ pyramid_frontend/tests/app.py
@@ -1,6 +1,16 @@
+import os
+import os.path
+import glob
+import pkg_resources
+import shutil
+
 from pyramid.config import Configurator
 
 from pyramid_frontend.tests.example import base, foo, bar
+from pyramid_frontend.images.files import save_image
+
+
+samples_dir = pkg_resources.resource_filename('pyramid_frontend.tests', 'data')
 
 
 def noop_view(request):
@@ -15,13 +25,46 @@ def bad_template_view(request):
     return {}
 
 
+def image_url_view(request):
+    load_images(request)
+    return request.image_url('smiley-jpeg-rgb', 'jpg', 'thumb')
+
+
+def load_images(request):
+    """
+    Load images from test 'samples' directory to originals dir.
+    """
+    settings = request.registry.settings
+    if settings.get('images-loaded'):
+        return
+    settings['images-loaded'] = True
+
+    originals = settings['pyramid_frontend.original_image_dir']
+    processed = settings['pyramid_frontend.processed_image_dir']
+    if os.path.exists(originals):
+        shutil.rmtree(originals)
+    if os.path.exists(processed):
+        shutil.rmtree(processed)
+    os.makedirs(originals)
+    os.makedirs(processed)
+
+    for filename in glob.glob(os.path.join(samples_dir, '*.*')):
+        f = open(filename, 'rb')
+        name, original_ext = os.path.basename(filename).rsplit('.', 1)
+        save_image(settings, name, original_ext, f)
+
+
 def make_app():
     settings = {
         'pyramid_frontend.theme': 'foo',
+        'pyramid_frontend.original_image_dir': '/tmp/pfe-image-originals',
+        'pyramid_frontend.processed_image_dir': '/tmp/pfe-image-processed',
     }
     config = Configurator(settings=settings)
 
     config.include('pyramid_frontend')
+
+    config.add_image_filter('thumb', width=200, height=200, crop=True)
 
     config.add_theme('base', base.BaseTheme)
     config.add_theme('foo', foo.FooTheme)
@@ -31,6 +74,7 @@ def make_app():
     config.add_route('article', '/article')
     config.add_route('bad-return', '/bad-return')
     config.add_route('bad-template', '/bad-template')
+    config.add_route('image-url', '/image-url')
 
     config.add_view(noop_view, route_name='index', renderer='index.html')
     config.add_view(noop_view, route_name='article', renderer='article.html')
@@ -38,6 +82,7 @@ def make_app():
                     renderer='index.html')
     config.add_view(bad_template_view, route_name='bad-template',
                     renderer='bad.html')
+    config.add_view(image_url_view, route_name='image-url', renderer='string')
 
     return config.make_wsgi_app()
 
--- pyramid_frontend/tests/test_functional.py
+++ pyramid_frontend/tests/test_functional.py
@@ -1,12 +1,15 @@
 from unittest import TestCase
+from cStringIO import StringIO
 
 from webtest import TestApp
 from pyramid.mako_templating import MakoRenderingException
 
+from PIL import Image
+
 from .app import make_app
 
 
-class TestFunctional(TestCase):
+class TestTemplatingFunctional(TestCase):
     def setUp(self):
         self.app = TestApp(make_app())
 
@@ -23,3 +26,16 @@ class TestFunctional(TestCase):
     def test_render_bad_template(self):
         with self.assertRaises(MakoRenderingException):
             self.app.get('/bad-template')
+
+
+class TestImagesFunctional(TestCase):
+    def setUp(self):
+        self.app = TestApp(make_app())
+
+    def test_fetch_image(self):
+        url_resp = self.app.get('/image-url')
+        img_resp = self.app.get(url_resp.body)
+        # This image should be 200x200
+        f = StringIO(img_resp.body)
+        im = Image.open(f)
+        self.assertEqual(im.size, (200, 200))

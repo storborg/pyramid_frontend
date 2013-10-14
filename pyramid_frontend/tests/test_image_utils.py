69531e70ba1cc647f41d91628335f04ece0259a8
Scott Torborg <scott@cartlogic.com>
2013-10-14 01:42:48 -0700

    Round out image utils tests

--- pyramid_frontend/tests/test_image_utils.py
+++ pyramid_frontend/tests/test_image_utils.py
@@ -17,6 +17,51 @@ class TestUtils(TestCase):
     def test_bounding_box(self):
         self.assertEqual(utils.bounding_box(self.im), (8, 16, 496, 504))
 
+    def test_image_entropy(self):
+        entropy = utils.image_entropy(self.im)
+        self.assertAlmostEqual(entropy, 5.4, places=1)
+
+    def test_is_larger(self):
+        self.assertTrue(utils.is_larger(self.im, (200, 200)))
+        self.assertFalse(utils.is_larger(self.im, (600, 600)))
+        self.assertFalse(utils.is_larger(self.im, (512, 512)))
+
+    def test_pad_image(self):
+        padded = utils.pad_image(self.im, (600, 600))
+        self.assertEqual(padded.size, (600, 600))
+
+    def test_colors_differ(self):
+        self.assertTrue(utils.colors_differ((127, 243, 100),
+                                            (125, 243, 95),
+                                            tolerance=5))
+        self.assertFalse(utils.colors_differ((127, 243, 100),
+                                             (125, 243, 95),
+                                             tolerance=20))
+
+    def test_crop_entropy_same(self):
+        im = utils.crop_entropy(self.im, (300, 300))
+        self.assertEqual(im.getpixel((150, 150)), (206, 205, 1))
+
+    def test_crop_entropy_wide(self):
+        im = utils.crop_entropy(self.im, (300, 100))
+        self.assertEqual(im.getpixel((150, 50)), (202, 200, 1))
+
+    def test_crop_entropy_tall(self):
+        im = utils.crop_entropy(self.im, (100, 300))
+        self.assertEqual(im.getpixel((50, 150)), (208, 208, 0))
+
+    def test_is_white_background(self):
+        self.assertTrue(utils.is_white_background(self.im))
+
+        cropped = self.im.crop((0, 0, 256, 256))
+        self.assertFalse(utils.is_white_background(cropped))
+        cropped = self.im.crop((0, 256, 256, 512))
+        self.assertFalse(utils.is_white_background(cropped))
+        cropped = self.im.crop((0, 0, 256, 512))
+        self.assertFalse(utils.is_white_background(cropped))
+        cropped = self.im.crop((256, 0, 512, 512))
+        self.assertFalse(utils.is_white_background(cropped))
+
 
 class TestFilters(TestCase):
     def test_png_save_rgb(self):

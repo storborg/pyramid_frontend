aff038511bde24e66bd9b0dfc9eac296133c11dc
Scott Torborg <scott@cartlogic.com>
2013-10-15 23:11:34 -0700

    Remove debugging statements

--- pyramid_frontend/assets/__init__.py
+++ pyramid_frontend/assets/__init__.py
@@ -70,5 +70,4 @@ def includeme(config):
     compiled_path = \
         config.registry.settings['pyramid_frontend.compiled_asset_dir']
     compiled_path = '/Users/scott/gallery/compiled/'
-    print "Compiled_path is %r" % compiled_path
     config.add_static_view(name='compiled', path=compiled_path)

229b7252710413292f9c6d44436d196cad379877
Scott Torborg <scott@cartlogic.com>
2013-10-19 13:06:29 -0700

    Round out compilation tests: 100% branch coverage now, but tests need solidifying

--- pyramid_frontend/tests/test_compile.py
+++ pyramid_frontend/tests/test_compile.py
@@ -1,8 +1,14 @@
+import os.path
+import subprocess
 from unittest import TestCase
 
 from .. import compile
+from ..assets.compiler import Compiler
+from ..assets.less import LessCompiler
+from ..assets.requirejs import RequireJSCompiler
 
 from . import utils
+from .example import foo
 
 
 class TestCompileCommand(TestCase):
@@ -17,3 +23,49 @@ class TestCompileCommand(TestCase):
     def test_pcompile(self):
         retcode = utils.compile_assets()
         self.assertEqual(retcode, 0)
+
+
+class TestCompiler(TestCase):
+    def setUp(self):
+        self.theme = foo.FooTheme({})
+        self.output_dir = os.path.join(utils.work_dir, 'compile-tests')
+
+    def test_bad_shell(self):
+        compiler = Compiler(None, '.')
+        with self.assertRaises(subprocess.CalledProcessError):
+            argv = [
+                'false',
+            ]
+            compiler.run_command(argv)
+            # XXX Try to test that this actually prints the stdout output of a
+            # failed comamnd.
+
+    def test_less_compile(self):
+        compiler = LessCompiler(self.theme, self.output_dir)
+        path = compiler.compile('main-less', '/_foo/css/main.less')
+        f = open(path, 'rb')
+        buf_minified = f.read()
+        self.assertGreater(len(buf_minified), 0)
+
+        # Test that minified version is smaller than non-minified.
+        compiler = LessCompiler(self.theme, self.output_dir, minify=False)
+        path = compiler.compile('main-less', '/_foo/css/main.less')
+        f = open(path, 'rb')
+        buf_unminified = f.read()
+
+        self.assertLess(len(buf_minified), len(buf_unminified))
+
+    def test_requirejs_compile(self):
+        compiler = RequireJSCompiler(self.theme, self.output_dir)
+        path = compiler.compile('main-js', '/_foo/js/main.js')
+        f = open(path, 'rb')
+        buf_minified = f.read()
+        self.assertGreater(len(buf_minified), 0)
+
+        # Test that minified version is smaller than non-minified.
+        compiler = RequireJSCompiler(self.theme, self.output_dir, minify=False)
+        path = compiler.compile('main-js', '/_foo/js/main.js')
+        f = open(path, 'rb')
+        buf_unminified = f.read()
+
+        self.assertLess(len(buf_minified), len(buf_unminified))

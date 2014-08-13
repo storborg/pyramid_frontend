af00f19ef32f816beaa2297b565a02222c08d0d0
Scott Torborg <scott@cartlogic.com>
2014-08-13 01:01:52 -0700

    Append to changelog, bump version, and prepare for a release

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class build(_build):
 
 setup(name='pyramid_frontend',
       cmdclass={'build': build},
-      version='0.3.1',
+      version='0.4',
       description='Themes, image filtering, and frontend asset handling.',
       long_description='',
       classifiers=[

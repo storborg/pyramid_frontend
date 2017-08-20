6b7da0bbda76360f22fa95c8bb1a38c59920e16a
Scott Torborg <storborg@gmail.com>
2017-08-19 21:43:12 -0700

    Bump version 0.6.5

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class build(_build):
 
 setup(name='pyramid_frontend',
       cmdclass={'build': build},
-      version='0.6.4',
+      version='0.6.5',
       description='Themes, image filtering, and frontend asset handling.',
       long_description='',
       classifiers=[
